b1b030e6300cf42ac74c3d3f6ab42aef56f91ec2
sudha <Sudhaas2802@gmail.com>
2025-07-02 11:34:03 -0400

    vpc module

--- peering.tf
+++ peering.tf
@@ -13,19 +13,19 @@ resource "aws_vpc_peering_connection" "default" {
   )
 }
 
-resource "aws_route" "public" {
+resource "aws_route" "public_route" {
   count = var.is_vpc_peering ? 1 : 0  
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
-resource "aws_route" "private" {
+resource "aws_route" "private_route" {
   count = var.is_vpc_peering ? 1 : 0  
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }  
-resource "aws_route" "database" {
+resource "aws_route" "database_route" {
   count = var.is_vpc_peering ? 1 : 0  
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = local.default_vpc_cidr
