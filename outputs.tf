0b9cc50ca89d51ce91954608b3a3e4d48c64de71
sudha <Sudhaas2802@gmail.com>
2025-07-02 11:22:02 -0400

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,15 @@
 # output "subnets_info" {
 #     value = aws_subnet.public
 # }
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
